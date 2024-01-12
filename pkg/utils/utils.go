8ef6cff25d44c909c7ce309082071a7d66376a33
AnjasFedo <fedoaf98@gmail.com>
2024-01-12 20:14:38 +0700

    Postman Collection & Some Explaination Comment

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,16 +1,18 @@
-package utils
+package utils  // Package declaration, utils package for utility functions
 
-import(
-	"encoding/json"
-	"io"
-	"net/http"
+import (
+	"encoding/json"  // Importing the encoding/json package for JSON encoding and decoding
+	"io/ioutil"      // Importing the ioutil package for reading from and writing to streams
+	"net/http"       // Importing the net/http package for HTTP functionality
 )
 
+// ParseBody function reads and parses the request body into the provided interface
 func ParseBody(r *http.Request, x interface{}) {
-	if body, err := io.ReadAll(r.Body); err != nil {
+	// Reading the entire request body
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		// Attempting to unmarshal (parse) the JSON body into the provided interface
 		if err := json.Unmarshal([]byte(body), x); err != nil {
-			return
+			return  // If there is an error during unmarshalling, return without modifying the provided interface
 		}
 	}
 }
-
